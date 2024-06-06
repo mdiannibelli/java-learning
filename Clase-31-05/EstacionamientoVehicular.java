667aecb4d1e7e07f0b642e6d3a65079735383be5
Marcos <mdiannibelli@gmail.com>
2024-06-05 21:00:17 -0300

    Clase 31 exercise

--- Clase-31-05/EstacionamientoVehicular.java
+++ Clase-31-05/EstacionamientoVehicular.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class EstacionamientoVehicular {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
         String plates[] = new String[8];
         String drivers[] = new String[8];
         
@@ -11,6 +13,9 @@ public class EstacionamientoVehicular {
         drivers[1] = "Pedro A.";
         drivers[2] = "Juan S.";
 
+        
+        // Vehicles quantity
+        int vehiclesQuantity = 0;
         // Get last plate
         String lastPlate = "";
         for(int i = 0; i < plates.length; i++) {
@@ -18,8 +23,34 @@ public class EstacionamientoVehicular {
                 lastPlate = plates[i-1];
                 break;
             }
+            vehiclesQuantity++;
         }
         System.out.println(lastPlate);
+        System.out.println(vehiclesQuantity);
+
+        // Find a driver
+        String driverToFind;
+        boolean foundedDriver = false;
+        int driverPosition = 0;
+        System.out.println("Introduce the name of the driver you want to search");
+        driverToFind = scn.nextLine();
+        for(int k = 0; k < drivers.length; k++) {
+            String c = drivers[k];
+            if(c == null) continue;
+            if(c.toLowerCase().equals(driverToFind.toLowerCase())) {
+                foundedDriver = true;
+                driverPosition = k;
+                break;
+            }
+        }
+
+        if(foundedDriver) {
+            System.out.println("Driver founded! ");
+            System.out.println(drivers[driverPosition]);
+            System.out.println(plates[driverPosition]);
+        } else {
+            System.out.println("We can't found a driver");
+        }
 
     }
 }
