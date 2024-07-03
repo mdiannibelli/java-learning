f3a70fc6b785e7fecae454a7e5e116b0e8ecbf15
mdiannibelli <mdiannibelli@gmail.com>
2024-07-03 12:26:48 -0300

    Stand and declare winner

--- blackjack-final/Blackjack.java
+++ blackjack-final/Blackjack.java
@@ -5,33 +5,42 @@ public class Blackjack {
         // All BJ Cards
         String cards[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
-        String userCards[] = new String[2];
-        String crupierCards[] = new String[2];
+        String userCards[] = new String[5];
+        int userCardsValue1 = 0;
+        int userCardsValue2 = 0;
+
+        String crupierCards[] = new String[5];
+        int crupierCardsValue1 = 0;
+        int crupierCardsValue2 = 0;
+
         String userMsg = "";
         String crupierMsg = "";
 
         boolean winner = false;
 
         JOptionPane.showMessageDialog(null, "Dealing cards to the user..", "Phase 1", JOptionPane.INFORMATION_MESSAGE);
-        generateRandomCards(cards, userCards);
-        Object[] userResult = showUserCardsValue(userCards);
+        System.out.println("Dealing cards to the user..");
+        generateRandomCards(cards, userCards, 2);
+        Object[] userResult = showUserCardsValue(userCards, userCardsValue1, userCardsValue2);
         userMsg = (String) userResult[0];
+        userCardsValue1 += (int) userResult[1];
+        userCardsValue2 += (int) userResult[2];
 
         // Check if user has Blackjack
-        winner = checkBlackjack((int) userResult[1], (int) userResult[2], 0, 0);
+        winner = checkBlackjack(userCardsValue1, userCardsValue2, crupierCardsValue1, crupierCardsValue2);
         if (winner)
             return;
 
         JOptionPane.showMessageDialog(null, "Dealing cards to the crupier..", "Phase 2",
                 JOptionPane.INFORMATION_MESSAGE);
-        generateRandomCards(cards, crupierCards);
-        Object[] crupierResult = showCrupierCardsValue(crupierCards);
+        System.out.println("Dealing cards to the crupier..");
+        generateRandomCards(cards, crupierCards, 1);
+        boolean hasA = false;
+        Object[] crupierResult = showCrupierCardsValue(crupierCards, crupierCardsValue1, crupierCardsValue2, hasA);
+        hasA = (boolean) crupierResult[3];
         crupierMsg = (String) crupierResult[0];
-
-        // Check if crupier has Blackjack
-        winner = checkBlackjack(0, 0, (int) crupierResult[1], (int) crupierResult[2]);
-        if (winner)
-            return;
+        crupierCardsValue1 += (int) crupierResult[1];
+        crupierCardsValue2 += (int) crupierResult[2];
 
         String options[] = { "Request card", "Stand" };
         var selection = JOptionPane.showOptionDialog(
@@ -50,10 +59,30 @@ public class Blackjack {
 
         if (selection == 1) {
             // Generate crupier card;
+            JOptionPane.showMessageDialog(null, "Dealing cards to the crupier..", "Phase 3",
+                    JOptionPane.INFORMATION_MESSAGE);
+            do {
+                System.out.println("Dealing cards to the crupier..");
+                generateRandomCards(cards, crupierCards, 1);
+                Object[] crupierNewCard = showCrupierCardsValue(crupierCards, crupierCardsValue1, crupierCardsValue2,
+                        hasA);
+                crupierCardsValue1 = (int) crupierNewCard[1];
+                crupierCardsValue2 = (int) crupierNewCard[2];
+                hasA = (boolean) crupierNewCard[3];
+
+                // Check if crupier has Blackjack
+                winner = checkBlackjack(userCardsValue1, userCardsValue2, crupierCardsValue1, crupierCardsValue2);
+                if (winner)
+                    return;
+            } while (crupierCardsValue1 < 17 | crupierCardsValue2 < 17);
+
+            // Show Winner
+            showWinner(crupierCardsValue1, crupierCardsValue2, userCardsValue1, userCardsValue2);
+
         }
     }
 
-    public static void generateRandomCards(String cards[], String personCards[]) {
+    public static void generateRandomCards(String cards[], String personCards[], int quantity) {
         // Give the cards to the person (user or crupier)
         int counter = 0;
         do {
@@ -63,80 +92,86 @@ public class Blackjack {
             JOptionPane.showMessageDialog(null, personCards[counter], "Card " + (counter + 1) + ": ",
                     JOptionPane.INFORMATION_MESSAGE);
             counter++;
-        } while (counter < 2);
+        } while (counter < quantity);
     }
 
-    public static Object[] showUserCardsValue(String userCards[]) {
+    public static Object[] showUserCardsValue(String userCards[], int personValue1, int personValue2) {
         // Show user cards value
-        int userCardsValue = 0;
         boolean hasA = false;
-        int userCardsValue2 = 0;
         for (int i = 0; i < userCards.length; i++) {
+            if (userCards[i] == null)
+                break;
             if (userCards[i] == "K" || userCards[i] == "J" || userCards[i] == "Q") {
-                userCardsValue += 10;
+                personValue2 += 10;
+                personValue1 += 10;
             } else if (userCards[i] == "A") {
-                userCardsValue2 += 1;
-                userCardsValue += 11;
+                personValue2 += 1;
+                personValue1 += 11;
                 hasA = true;
             } else {
-                userCardsValue += Integer.parseInt(userCards[i]);
+                personValue1 += Integer.parseInt(userCards[i]);
+                personValue2 += Integer.parseInt(userCards[i]);
             }
         }
 
         if (userCards[0] == "A" && userCards[1] == "A") {
-            userCardsValue2 = 2;
-        } else {
-            userCardsValue2 = userCardsValue2 + (userCardsValue - 11);
+            personValue2 = 2;
+        } else if (userCards[0] == "A" && userCards[1] == "A" && userCards[2] == "A") {
+            personValue2 = 3;
         }
 
         String userMsg;
         if (hasA) {
-            userMsg = "Your cards add up " + Integer.toString(userCardsValue) + "/" + Integer.toString(userCardsValue2);
+            userMsg = "Your cards add up " + Integer.toString(personValue1) + "/" + Integer.toString(personValue2);
             JOptionPane.showMessageDialog(null, userMsg, "Total value:",
                     JOptionPane.INFORMATION_MESSAGE);
         } else {
-            userMsg = "Your cards add up " + userCardsValue;
-            JOptionPane.showMessageDialog(null, "Your cards add up " + userCardsValue, "Total value:",
+            userMsg = "Your cards add up " + personValue1;
+            JOptionPane.showMessageDialog(null, "Your cards add up " + personValue1, "Total value:",
                     JOptionPane.INFORMATION_MESSAGE);
         }
-        return new Object[] { userMsg, userCardsValue, userCardsValue2 };
+        return new Object[] { userMsg, personValue1, personValue2 };
     }
 
-    public static Object[] showCrupierCardsValue(String crupierCards[]) {
+    public static Object[] showCrupierCardsValue(String crupierCards[], int personValue1, int personValue2,
+            boolean hasA) {
         // Show crupier cards value
-        int crupierCardsValue = 0;
-        boolean hasA = false;
-        int crupierCardsValue2 = 0;
+        // boolean hasA = false;
         for (int i = 0; i < crupierCards.length; i++) {
+            if (crupierCards[i] == null)
+                break;
             if (crupierCards[i] == "K" || crupierCards[i] == "J" || crupierCards[i] == "Q") {
-                crupierCardsValue += 10;
+                personValue1 += 10;
+                personValue2 += 10;
             } else if (crupierCards[i] == "A") {
-                crupierCardsValue2 += 1;
-                crupierCardsValue += 11;
+                personValue2 += 1;
+                personValue1 += 11;
                 hasA = true;
             } else {
-                crupierCardsValue += Integer.parseInt(crupierCards[i]);
+                personValue1 += Integer.parseInt(crupierCards[i]);
+                personValue2 += Integer.parseInt(crupierCards[i]);
             }
+
         }
 
         if (crupierCards[0] == "A" && crupierCards[1] == "A") {
-            crupierCardsValue2 = 2;
-        } else {
-            crupierCardsValue2 = crupierCardsValue2 + (crupierCardsValue - 11);
+            personValue2 = 2;
+        } else if (crupierCards[0] == "A" && crupierCards[1] == "A" && crupierCards[2] == "A") {
+            personValue2 = 3;
         }
 
         String crupierMsg;
-        if (hasA) {
-            crupierMsg = "Crupier cards add up " + Integer.toString(crupierCardsValue) + "/"
-                    + Integer.toString(crupierCardsValue2);
+        if (hasA && personValue1 != 21) {
+            crupierMsg = "Crupier cards add up " + Integer.toString(personValue1) + "/"
+                    + Integer.toString(personValue2);
             JOptionPane.showMessageDialog(null, crupierMsg, "Total value:",
                     JOptionPane.INFORMATION_MESSAGE);
         } else {
-            crupierMsg = "Crupier cards add up " + crupierCardsValue;
-            JOptionPane.showMessageDialog(null, "Crupier cards add up " + crupierCardsValue, "Total value:",
+            crupierMsg = "Crupier cards add up " + personValue1;
+            JOptionPane.showMessageDialog(null, "Crupier cards add up " + personValue1, "Total value:",
                     JOptionPane.INFORMATION_MESSAGE);
         }
-        return new Object[] { crupierMsg, crupierCardsValue, crupierCardsValue2 };
+        return new Object[] { crupierMsg, personValue1, personValue2, hasA };
     }
 
     public static boolean checkBlackjack(int userCardsValue, int userCardsValue2, int crupierCardsValue,
@@ -162,4 +197,25 @@ public class Blackjack {
         return false;
     }
 
+    public static void showWinner(int crupierCardsValue1, int crupierCardsValue2, int userCardsValue1,
+            int userCardsValue2) {
+        if (crupierCardsValue1 > 21 && crupierCardsValue2 > 21) {
+            JOptionPane.showMessageDialog(null, "Crupier is over 21, user win!", "Game over",
+                    JOptionPane.INFORMATION_MESSAGE);
+            System.out.println("Crupier is over 21, user win!");
+            return;
+        } else {
+            if (crupierCardsValue1 > userCardsValue1 | crupierCardsValue2 > userCardsValue2) {
+                JOptionPane.showMessageDialog(null, "Crupier won!", "Game over",
+                        JOptionPane.INFORMATION_MESSAGE);
+                System.out.println("Crupier won!");
+                return;
+            } else {
+                JOptionPane.showMessageDialog(null, "User won!", "Game over",
+                        JOptionPane.INFORMATION_MESSAGE);
+                System.out.println("User won!");
+                return;
+            }
+        }
+    }
 }
